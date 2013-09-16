00d71a74c6151b726f4cc6eaab9f426697809a64
asrivas <asrivastava274@gmail.com>
2013-09-15 20:46:02 -0700

    generate welcome index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '8d2983f5b8e3155551dbff364b1ee3b60fb8a3b236e639b5e143fe2238b5eabb0deba736c53a0014fb3a25022dcee43aa0464391945d7535e2a57a0c77e77594'
